b7374128fdec7f0a21c4e40eb3e8310b6ba340c2
luanmcarvalho <luanmcarvalho@hotmail.com>
2025-01-24 17:27:31 -0300

    jump function added

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main ()
     const int windowWidth = 1280;
     const int WindowsHeight = 720;
 
+    // Gravity
+    const int gravity = 1;
+
     InitWindow(windowWidth, WindowsHeight, "Dapper Dasher");
 
     //rectangle dimensions
@@ -19,13 +22,23 @@ int main ()
 
     while (!WindowShouldClose())
     {
+        //start drawing
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-          if (IsKeyPressed(KEY_SPACE))
+        if (posY >= WindowsHeight - height)
+        {
+            vel = 0;
+        }
+        else
+        {
+            vel += gravity;
+        }
+
+          if (IsKeyPressed(KEY_SPACE) && posY >= WindowsHeight - height)
         {
             // Update Velocity
-            vel -= 10;
+            vel -= 20;
         }
 
         posY += vel;
