3b6658b316cee45d035ef2de84c847f90db5a13c
luanmcarvalho <luanmcarvalho@hotmail.com>
2025-01-30 14:08:29 -0300

    scarfy texture added

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@ int main ()
 {
     SetTargetFPS(60);
 
-    // Window Size
+      // Window Size
     const int windowWidth = 1280;
     const int WindowsHeight = 720;
+
+    InitWindow(windowWidth, WindowsHeight, "Dapper Dasher");
+
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle Recscarfy;
+
+    // Set the dimensions of the rectangle
+    Recscarfy.height = scarfy.height;
+    Recscarfy.width = scarfy.width/6;
+    Recscarfy.x = 0;
+    Recscarfy.y = 0;
+
+    // Scarfy position
+    Vector2 scarfyPosition;
+    scarfyPosition.x = windowWidth/2 - Recscarfy.width/2;
+    scarfyPosition.y = WindowsHeight - Recscarfy.height;
 
     // Gravity
     const int gravity = 1;
 
-    InitWindow(windowWidth, WindowsHeight, "Dapper Dasher");
-
-    //rectangle dimensions
-    int width = 100;
-    int height = 100;
-
-    int  posY = WindowsHeight - height;
+    // Velocity
     int vel = 0;
 
     while (!WindowShouldClose())
@@ -26,7 +36,8 @@ int main ()
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        if (posY >= WindowsHeight - height)
+        // Ground check
+        if (scarfyPosition.y >= WindowsHeight - Recscarfy.height)
         {
             vel = 0;
         }
@@ -35,17 +46,19 @@ int main ()
             vel += gravity;
         }
 
-          if (IsKeyPressed(KEY_SPACE) && posY >= WindowsHeight - height)
+        // Jump
+          if (IsKeyPressed(KEY_SPACE) && scarfyPosition.y >= WindowsHeight - Recscarfy.height)
         {
             // Update Velocity
             vel -= 20;
         }
 
-        posY += vel;
+        scarfyPosition.y += vel;
 
-        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+        DrawTextureRec(scarfy, Recscarfy, scarfyPosition, WHITE);
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 
 }
