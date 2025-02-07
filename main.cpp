22a8c0ea07d08f5ea9aa46ed94e4cf1ec96228c7
luanmcarvalho <luanmcarvalho@hotmail.com>
2025-02-07 18:30:54 -0300

    finish line added

--- main.cpp
+++ main.cpp
@@ -68,20 +68,76 @@ int main ()
         nebulaE[i].position.x = windowWidth + i * 500;
     }
 
+    float finishLine = nebulaE[nebulaNum - 1].position.x;
+
 
     //nebula movement
     int nebulaVel = -300;
     //--------------------------------------------------------------
+    // Background setup
+    Texture2D background = LoadTexture("textures/far-buildings.png");
+    float bgX = 0.0;
+
+    Texture2D midgroung = LoadTexture("textures/back-buildings.png");
+    float mgX = 0.0;
+
+    Texture2D foreground = LoadTexture("textures/foreground.png");
+    float fgX = 0.0;
+
+    //colision
+    bool collision = false;
 
     // Game Loop----------------------------------------------------
     while (!WindowShouldClose())
     {
+        // Calculate Delta Time
+        const float dT = GetFrameTime();
+
         //start drawing
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Delta Time
-        const float dT = GetFrameTime();
+        // Background Drawing
+
+        bgX -= 40 * dT;
+        if (bgX <= -background.width * 4)
+        {
+            bgX = 0.0;
+        }
+
+        Vector2 bg1position {bgX, 0.0};
+        DrawTextureEx(background, bg1position, 0.0, 4.0, WHITE);
+
+        Vector2 bg2position {bgX + background.width * 4, 0.0, };
+        DrawTextureEx(background, bg2position, 0.0, 4.0, WHITE);
+
+        // Midground Drawing
+
+        mgX -= 80 * dT;
+        if (mgX <= -midgroung.width * 4)
+        {
+            mgX = 0.0;
+        }
+
+        Vector2 mg1position {mgX, 0.0};
+        DrawTextureEx(midgroung, mg1position, 0.0, 4.0, WHITE);
+
+        Vector2 mg2position {mgX + midgroung.width * 4, 0.0, };
+        DrawTextureEx(midgroung, mg2position, 0.0, 4.0, WHITE);
+
+        // Foreground Drawing
+
+        fgX -= 120 * dT;
+        if (fgX <= -foreground.width * 4)
+        {
+            fgX = 0.0;
+        }
+
+        Vector2 fg1position {fgX, 0.0};
+        DrawTextureEx(foreground, fg1position, 0.0, 4.0, WHITE);
+
+        Vector2 fg2position {fgX + foreground.width * 4, 0.0, };
+        DrawTextureEx(foreground, fg2position, 0.0, 4.0, WHITE);
 
         // Ground check
         if (scarfyData.position.y >= WindowsHeight - scarfyData.rec.height)
@@ -107,6 +163,8 @@ int main ()
         {
             nebulaE[i].position.x += nebulaVel * dT;
         }
+
+        finishLine += nebulaVel * dT;
 
         //scarfy Movement
         scarfyData.position.y += vel * dT;
@@ -149,14 +207,43 @@ int main ()
         }
         // End of Loop------------------------------------------------
 
-        // Draw
-        for (int i = 0; i < nebulaNum; i++)
+        for (animData nebula : nebulaE)
         {
-            DrawTextureRec(nebula, nebulaE[i].rec, nebulaE[i].position, WHITE);
+            float pad = 30;
+            Rectangle scarfyRec = {scarfyData.position.x + pad, scarfyData.position.y + pad, scarfyData.rec.width - pad * 2, scarfyData.rec.height - pad * 2};
+            Rectangle nebulaRec = {nebula.position.x, nebula.position.y, nebula.rec.width, nebula.rec.height};
+
+             if (CheckCollisionRecs(scarfyRec, nebulaRec))
+            {
+                collision = true;
+            }
         }
-        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.position, WHITE);
+
+        if (collision)
+        {
+            DrawText("Game Over", windowWidth/2 - 100, WindowsHeight/2, 40, RED);
+        }
+        else if(finishLine <= 0)
+        {
+            DrawText("You Win", windowWidth/2 - 100, WindowsHeight/2, 40, GREEN);
+        }
+        else
+        {
+            // Draw
+            for (int i = 0; i < nebulaNum; i++)
+            {
+                DrawTextureRec(nebula, nebulaE[i].rec, nebulaE[i].position, WHITE);
+            }
+
+            DrawTextureRec(scarfy, scarfyData.rec, scarfyData.position, WHITE);
+        }
+
+
         EndDrawing();
     }
+    UnloadTexture(background);
+    UnloadTexture(midgroung);
+    UnloadTexture(foreground);
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
     CloseWindow();
